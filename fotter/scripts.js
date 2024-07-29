d805f01f9ce785dc42a10c5df4fea96ffb0996e1
Shyanil <shyanilmishra94@gmail.com>
2024-07-29 20:05:36 +0530

    Shyanil 29-07-2024 footer added in the vivass

---
+++ fotter/scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+document.querySelector(".email-button").addEventListener("click", function () {
+  window.location.href = "mailto:example@example.com";
+});
+
+document.querySelector(".phone-button").addEventListener("click", function () {
+  window.location.href = "tel:+4414143459875";
+});
